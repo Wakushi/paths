5f113d7b7fc1f150bfceced9ee50c9f4e76dfba4
Wakushi <mzozord@gmail.com>
2023-05-26 12:23:55 +0200

    - Project initialization - Added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ChoiceComponent } from './choice/choice.component';
+import { GaugeComponent } from './gauge/gauge.component';
+import { GameOverComponent } from './game-over/game-over.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ChoiceComponent,
+    GaugeComponent,
+    GameOverComponent
   ],
   imports: [
     BrowserModule,
