a8c07c61d32217af83d8f5be84e87963b0c6a29c
Wakushi <mzozord@gmail.com>
2023-05-31 10:25:01 +0200

    - Added events

--- src/app/core/services/events-collection.ts
+++ src/app/core/services/events-collection.ts
@@ -238,4 +238,385 @@ export const events: EventModel[] = [
       consequence: { energy: -10 },
     },
   },
+  {
+    eventName: "Chronomécanicien Griggs",
+    dialog:
+      "Temporal engine stability is decreasing, should I engage the backup chronal buffer?",
+    eventImage: CHRONOMECANICIEN_GRIGGS_IMAGE,
+    rightChoice: {
+      text: "Yes, activate the chronal buffer!",
+      consequence: { time: 20, energy: -10 },
+    },
+    leftChoice: {
+      text: "No, let it be for now.",
+      consequence: { time: -15 },
+    },
+  },
+  {
+    eventName: "IA Méridiana",
+    dialog:
+      "I've detected a time anomaly. It could be beneficial to investigate, but it will require energy.",
+    eventImage: IA_MERIDIANA_IMAGE,
+    rightChoice: {
+      text: "Investigate the anomaly.",
+      consequence: { time: 15, energy: -20, mental: 5 },
+    },
+    leftChoice: {
+      text: "Ignore it, it's too risky.",
+      consequence: { mental: -10 },
+    },
+  },
+  {
+    eventName: "Commandant Thalia",
+    dialog:
+      "We are approaching a temporal rift. Shall we adjust our course to avoid it?",
+    eventImage: COMMANDANT_THALIA_IMAGE,
+    rightChoice: {
+      text: "Yes, avoid the rift!",
+      consequence: { time: -20, energy: 10 },
+    },
+    leftChoice: {
+      text: "No, pass through it!",
+      consequence: { time: 20, health: -20, energy: -20 },
+    },
+  },
+  {
+    eventName: "Ingénieur en Énergie Zoltan",
+    dialog:
+      "There's an energy surge in the temporal engine. Shall I reroute the extra energy?",
+    eventImage: INGENIEUR_ENERGIE_ZOLTAN_IMAGE,
+    rightChoice: {
+      text: "Reroute to shields!",
+      consequence: { health: 20, time: -10 },
+    },
+    leftChoice: {
+      text: "Reroute to mental capacity enhancement!",
+      consequence: { mental: 20, time: -10 },
+    },
+  },
+  {
+    eventName: "Navigator Kassiopeia",
+    dialog:
+      "We've detected a shortcut through time, but it could destabilize the ship. What's our course of action?",
+    eventImage: NAVIGATOR_KASSIOPEIA_IMAGE,
+    rightChoice: {
+      text: "Take the shortcut!",
+      consequence: { time: 30, health: -10, energy: -15 },
+    },
+    leftChoice: {
+      text: "Stay on the current path.",
+      consequence: { time: -10 },
+    },
+  },
+  {
+    eventName: "Commandant Thalia",
+    dialog: "We've stumbled upon an abandoned ship loaded with energy cells.",
+    eventImage: COMMANDANT_THALIA_IMAGE,
+    rightChoice: {
+      text: "Harvest the cells.",
+      consequence: { energy: 20, time: -5 },
+    },
+    leftChoice: {
+      text: "Ignore and move on.",
+      consequence: {},
+    },
+  },
+  {
+    eventName: "IA Méridiana",
+    dialog: "Detected an approaching time storm.",
+    eventImage: IA_MERIDIANA_IMAGE,
+    rightChoice: {
+      text: "Adjust shields to withstand it.",
+      consequence: { time: 15, energy: -10 },
+    },
+    leftChoice: {
+      text: "Change course to avoid it.",
+      consequence: { time: -10 },
+    },
+  },
+  {
+    eventName: "Chronomécanicien Griggs",
+    dialog: "Identified a potential upgrade in the temporal engine.",
+    eventImage: CHRONOMECANICIEN_GRIGGS_IMAGE,
+    rightChoice: {
+      text: "Apply the upgrade.",
+      consequence: { time: 20, energy: -15 },
+    },
+    leftChoice: {
+      text: "Maintain current configuration.",
+      consequence: {},
+    },
+  },
+  {
+    eventName: "Docteur Proxima",
+    dialog:
+      "Found a new medicinal herb on the latest planet. Can be useful for enhancing crew's physical health.",
+    eventImage: DOCTEUR_PROXIMA_IMAGE,
+    rightChoice: {
+      text: "Harvest and use it.",
+      consequence: { health: 20, time: -5 },
+    },
+    leftChoice: {
+      text: "Leave it, focus on the mission.",
+      consequence: {},
+    },
+  },
+  {
+    eventName: "Data Archiviste Altair",
+    dialog: "Potential breakthrough in understanding a new form of energy.",
+    eventImage: DATA_ARCHIVISTE_ALTAIR_IMAGE,
+    rightChoice: {
+      text: "Pursue the research.",
+      consequence: { energy: 20, time: -10, mental: 5 },
+    },
+    leftChoice: {
+      text: "Stick to known energy forms.",
+      consequence: { mental: -10 },
+    },
+  },
+  {
+    eventName: "Navigator Kassiopeia",
+    dialog: "Sensed a shortcut in our journey.",
+    eventImage: NAVIGATOR_KASSIOPEIA_IMAGE,
+    rightChoice: {
+      text: "Take the shortcut.",
+      consequence: { time: 15, health: -10 },
+    },
+    leftChoice: {
+      text: "Stick to the current path.",
+      consequence: { time: -5 },
+    },
+  },
+  {
+    eventName: "Architecte Stellaire Orion",
+    dialog:
+      "Our current trajectory will take us through a field of space debris.",
+    eventImage: ARCHITECTE_STELLAIRE_ORION_IMAGE,
+    rightChoice: {
+      text: "Activate additional shields.",
+      consequence: { health: 10, energy: -10 },
+    },
+    leftChoice: {
+      text: "Change course to avoid it.",
+      consequence: { time: -10 },
+    },
+  },
+  {
+    eventName: "Ingénieur en Énergie Zoltan",
+    dialog: "Extra energy is being wasted. Possible to reroute.",
+    eventImage: INGENIEUR_ENERGIE_ZOLTAN_IMAGE,
+    rightChoice: {
+      text: "Reroute to mental capacity enhancement.",
+      consequence: { mental: 20, energy: -10 },
+    },
+    leftChoice: {
+      text: "Let it be.",
+      consequence: {},
+    },
+  },
+  {
+    eventName: "Commandant Thalia",
+    dialog:
+      "A mysterious object is floating near the ship. It could contain valuable resources.",
+    eventImage: COMMANDANT_THALIA_IMAGE,
+    rightChoice: {
+      text: "Retrieve the object.",
+      consequence: { energy: 15, health: -5 },
+    },
+    leftChoice: {
+      text: "It's not part of our mission. Ignore it.",
+      consequence: {},
+    },
+  },
+  {
+    eventName: "IA Méridiana",
+    dialog:
+      "A sudden spike in the temporal matrix could mean a potential time jump.",
+    eventImage: IA_MERIDIANA_IMAGE,
+    rightChoice: {
+      text: "Perform a manual override to stabilize.",
+      consequence: { time: 15, mental: -10 },
+    },
+    leftChoice: {
+      text: "Let it pass. It's a normal glitch.",
+      consequence: { time: -15 },
+    },
+  },
+  {
+    eventName: "Chronomécanicien Griggs",
+    dialog:
+      "A temporal loophole has been detected. We can exploit it for fast travel.",
+    eventImage: CHRONOMECANICIEN_GRIGGS_IMAGE,
+    rightChoice: {
+      text: "Use the loophole.",
+      consequence: { time: 30, energy: -20, health: -10 },
+    },
+    leftChoice: {
+      text: "Stay in normal space-time.",
+      consequence: { time: -10 },
+    },
+  },
+  {
+    eventName: "Navigator Kassiopeia",
+    dialog:
+      "Star maps indicate an uncharted planet on our route. It might hold unknown risks.",
+    eventImage: NAVIGATOR_KASSIOPEIA_IMAGE,
+    rightChoice: {
+      text: "Alter course to investigate the planet.",
+      consequence: { time: -15, energy: -10, mental: 10 },
+    },
+    leftChoice: {
+      text: "Maintain current course.",
+      consequence: {},
+    },
+  },
+  {
+    eventName: "Docteur Proxima",
+    dialog:
+      "A space-borne virus has been detected onboard. We need to act fast.",
+    eventImage: DOCTEUR_PROXIMA_IMAGE,
+    rightChoice: {
+      text: "Initiate the containment protocol.",
+      consequence: { health: 20, time: -10 },
+    },
+    leftChoice: {
+      text: "Isolate affected areas and continue the mission.",
+      consequence: { health: -20 },
+    },
+  },
+  {
+    eventName: "Architecte Stellaire Orion",
+    dialog:
+      "A nearby star is about to go supernova. We can harness the energy, but it's risky.",
+    eventImage: ARCHITECTE_STELLAIRE_ORION_IMAGE,
+    rightChoice: {
+      text: "Harness the energy.",
+      consequence: { energy: 40, health: -20, time: -15 },
+    },
+    leftChoice: {
+      text: "Move away from the star.",
+      consequence: { time: -10 },
+    },
+  },
+  {
+    eventName: "Data Archiviste Altair",
+    dialog:
+      "We've detected an ancient data capsule. It could boost Méridiana's abilities.",
+    eventImage: DATA_ARCHIVISTE_ALTAIR_IMAGE,
+    rightChoice: {
+      text: "Integrate the data.",
+      consequence: { mental: 20, energy: -10 },
+    },
+    leftChoice: {
+      text: "It's too risky. Leave it.",
+      consequence: {},
+    },
+  },
+  {
+    eventName: "Ingénieur en Énergie Zoltan",
+    dialog:
+      "We've got an energy leak in the auxiliary engine. We need to fix it now!",
+    eventImage: INGENIEUR_ENERGIE_ZOLTAN_IMAGE,
+    rightChoice: {
+      text: "Fix it immediately.",
+      consequence: { energy: 15, time: -10 },
+    },
+    leftChoice: {
+      text: "We can manage. Fix it later.",
+      consequence: { energy: -15 },
+    },
+  },
+  {
+    eventName: "Commandant Thalia",
+    dialog:
+      "There's a strange creature outside. It seems to be attracted to our energy source.",
+    eventImage: COMMANDANT_THALIA_IMAGE,
+    rightChoice: {
+      text: "Try to communicate.",
+      consequence: { mental: 10, energy: -5 },
+    },
+    leftChoice: {
+      text: "Ignore it and move on.",
+      consequence: {},
+    },
+  },
+  {
+    eventName: "IA Méridiana",
+    dialog:
+      "We've encountered a dimensional rift. We can either navigate around it or go through it.",
+    eventImage: IA_MERIDIANA_IMAGE,
+    rightChoice: {
+      text: "Navigate through it.",
+      consequence: { time: 30, energy: -20, health: -15 },
+    },
+    leftChoice: {
+      text: "Go around it.",
+      consequence: { time: -10 },
+    },
+  },
+  {
+    eventName: "Chronomécanicien Griggs",
+    dialog: "A temporal storm is approaching. We need to take action.",
+    eventImage: CHRONOMECANICIEN_GRIGGS_IMAGE,
+    rightChoice: {
+      text: "Activate the temporal shields.",
+      consequence: { time: 20, energy: -15 },
+    },
+    leftChoice: {
+      text: "Change the course to avoid the storm.",
+      consequence: { time: -15 },
+    },
+  },
+  {
+    eventName: "Navigator Kassiopeia",
+    dialog: "There's a black hole in our path. We need to change the course.",
+    eventImage: NAVIGATOR_KASSIOPEIA_IMAGE,
+    rightChoice: {
+      text: "Change course immediately.",
+      consequence: { time: -10 },
+    },
+    leftChoice: {
+      text: "Use its gravity to boost our speed.",
+      consequence: { time: 30, energy: -20, health: -10 },
+    },
+  },
+  {
+    eventName: "Docteur Proxima",
+    dialog: "The crew is stressed out. We need a break.",
+    eventImage: DOCTEUR_PROXIMA_IMAGE,
+    rightChoice: {
+      text: "Give the crew a day off.",
+      consequence: { health: 15, time: -10 },
+    },
+    leftChoice: {
+      text: "The mission must continue.",
+      consequence: { health: -10 },
+    },
+  },
+  {
+    eventName: "Docteur Proxima",
+    dialog: "There's an unidentified disease spreading. We need to quarantine.",
+    eventImage: DOCTEUR_PROXIMA_IMAGE,
+    rightChoice: {
+      text: "Initiate quarantine protocols.",
+      consequence: { health: 20, time: -15, energy: -10 },
+    },
+    leftChoice: {
+      text: "Keep working on a cure.",
+      consequence: { health: -20 },
+    },
+  },
+  {
+    eventName: "IA Méridiana",
+    dialog: "I need a system upgrade. It can improve my performance.",
+    eventImage: IA_MERIDIANA_IMAGE,
+    rightChoice: {
+      text: "Proceed with the upgrade.",
+      consequence: { mental: 20, time: -10, energy: -10 },
+    },
+    leftChoice: {
+      text: "You're functioning well enough. No need for an upgrade.",
+      consequence: {},
+    },
+  },
 ]
